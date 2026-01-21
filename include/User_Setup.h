43d983ce388adb044c79a743495b58d77e0ea04f
gustafpeter330-source <gustafpeter330@gmail.com>
2026-01-21 20:21:10 +0100

    neu

--- include/User_Setup.h
+++ include/User_Setup.h
@@ -4,7 +4,7 @@
 // ======================================================
 
 #define ST7789_DRIVER      // ST7789 Treiber
-
+#define USE_HSPI_PORT    // HSPI Port verwenden
 #define TFT_WIDTH  240
 #define TFT_HEIGHT 320
 
@@ -12,14 +12,14 @@
 #define TFT_MOSI  11
 #define TFT_SCLK  12
 #define TFT_MISO  -1        // Nicht verwendet
-#define TFT_CS    10
-#define TFT_DC    9
-#define TFT_RST   14
+#define TFT_CS    15
+#define TFT_DC    16
+#define TFT_RST   17
 
-#define TFT_BL     48         // Backlight Pin
+#define TFT_BL     18         // Backlight Pin
 #define TFT_BACKLIGHT_ON HIGH
-
-#define SPI_FREQUENCY 10000000  // 10 MHz
+#define TFT_INVERSION_ON HIGH
+#define SPI_FREQUENCY 20000000  // 20 MHz
 
 // Optional: Displayinvertierung
 //#define TFT_INVERSION_ON
